7732e3cda53d5bd01ebdf26df95c3c5322d4526c
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-01-10 15:59:22 +0300

    Add switch_to_frame method and crawlaer can get a turtle card.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-
+from selenium.webdriver.common.by import By
 
 
 class MainCrawler:
@@ -7,7 +7,17 @@ class MainCrawler:
         self.driver = webdriver.Chrome()
         
     def scrap(self) -> None:
-        self.driver.get('https://www.scrapethissite.com/pages/frames/')    
-        
+        self.driver.get('https://www.scrapethissite.com/pages/frames/')
+        self.switch_to_frame()
+        turtle_card = self.driver.find_elements(By.CLASS_NAME, 'turtle-family-card')
+    
+    def switch_to_frame(self) -> None :
+        self.driver.switch_to.frame(self.driver.find_element(By.TAG_NAME, 'iframe'))
+                           
     def __del__(self):
         self.driver.close()
+
+
+if __name__ == '__main__':
+    crawler = MainCrawler()
+    crawler.scrap()
